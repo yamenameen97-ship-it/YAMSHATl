3330c5a81e3f924f16646b3c3c2284cc0d356079
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-05-11 22:59:55 +0000

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import { ToastProvider } from './components/admin/ToastProvider.jsx';
 import AppStatusBanner from './components/system/AppStatusBanner.jsx';
 import AppErrorBoundary from './components/system/AppErrorBoundary.jsx';
 import InstallPrompt from './components/feedback/InstallPrompt.jsx';
+import OfflineExperience from './components/feedback/OfflineExperience.jsx';
 import { RoutePageSkeleton } from './components/feedback/Skeleton.jsx';
 import useNetworkStatus from './hooks/useNetworkStatus.js';
 import useOfflineQueue from './hooks/useOfflineQueue.js';
@@ -95,6 +96,7 @@ function AppGuards() {
     <>
       <AppStatusBanner />
       <InstallPrompt />
+      <OfflineExperience />
       {activeRequests > 0 ? <div className="global-progress-bar" /> : null}
     </>
   );

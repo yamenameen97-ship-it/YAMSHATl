e1c192b80c2dd69d9d210d051b2bbd7b6aeccf3b
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-05-13 08:25:36 +0300

    Delete frontend directory

--- frontend/dist/chunks/chat-LkrMux9V.js
+++
@@ -1,1 +0,0 @@
-import r from"./Chat-X3FlGn_6.js";import t from"./Inbox-BNAWxaMM.js";export{r as Chat,t as Inbox};
--- frontend/dist/chunks/notifications-D3YAbHb6.js
+++
@@ -1,1 +0,0 @@
-import i from"./Notifications-DxxChhUq.js";export{i as Notifications};

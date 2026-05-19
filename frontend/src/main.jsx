0113caac0be7cefa52679267a440a0e005a4cc44
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-05-19 05:50:47 +0300

    Import service worker manager in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,3 +1,4 @@
+import { registerServiceWorker } from './service-worker-manager';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';

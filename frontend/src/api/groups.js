9e15965e5aed3046a9f592ff27c6699db1a83e2a
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-05-22 23:50:14 +0300

    YSDE
    DDDGF

--- frontend/src/api/groups.js
+++
@@ -1,5 +0,0 @@
-import API from './axios.js';
-
-export const getGroups = () => API.get('/groups');
-export const createGroup = (payload) => API.post('/groups', payload);
-export const joinGroup = (groupId) => API.post(`/groups/${groupId}/join`);
--- frontend/src/components/feed/EmptyFeedState.jsx
+++
@@ -1,7 +0,0 @@
-export default function EmptyFeedState() {
-  return (
-    <div className="text-center p-8">
-      No posts yet.
-    </div>
-  );
-}

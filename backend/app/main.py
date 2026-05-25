22d0eacf857e4e6c51181bc85781aee212dc210f
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-05-26 01:59:15 +0300

    Fix formatting of allowed origins in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -56,11 +56,10 @@ fastapi_app.add_middleware(
         'http://localhost: 5173',
         ' http://127.0.0.1:5173',
         ' https://yamshatl-1-yg1o.onrender.com'
-        ' https://yamshatl-ahj8.onrender.com'
+        ' https://yamshatl-ahj8.onrender.com'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
-]
 )
 
 if settings.ENABLE_METRICS:

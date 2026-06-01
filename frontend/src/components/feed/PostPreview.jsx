692a9762fa969dc0160f48215f7590949100869d
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-06-01 03:58:05 +0300

    jhkj
    766

--- frontend/src/components/feed/PostPreview.jsx
+++
@@ -1,8 +0,0 @@
-export default function PostPreview({ post }) {
-  return (
-    <div className="border rounded-xl p-4">
-      <h3>Preview</h3>
-      <p>{post?.text}</p>
-    </div>
-  );
-}
--- frontend/src/components/upload/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FileUploader } from './FileUploader.jsx';
-export { default as ImageUploader } from './ImageUploader.jsx';
-export { default as VideoUploader } from './VideoUploader.jsx';

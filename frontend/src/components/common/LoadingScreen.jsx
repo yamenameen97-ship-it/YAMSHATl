b90c379b12ec21c062dd8e771c408fe207d9e9df
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-06-06 07:43:19 +0300

    897
    89

--- frontend/src/components/common/LoadingScreen.jsx
+++
@@ -1,7 +0,0 @@
-export default function LoadingScreen() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div>Loading...</div>
-    </div>
-  );
-}
--- frontend/src/routes/authRoutes.js
+++
@@ -1,6 +0,0 @@
-export const authRoutes = [
-  '/login',
-  '/register',
-  '/forgot-password',
-  '/reset-password',
-];

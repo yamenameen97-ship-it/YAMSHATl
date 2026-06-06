66b73846512111cb38e4a566107dc1b92b22f180
yamenameen97-ship-it <yamenameen97@gmail.com>
2026-06-06 07:33:11 +0300

    534
    5

--- frontend/src/components/ui/BottomSheet.jsx
+++
@@ -1,5 +0,0 @@
-import Sheet from './Sheet.jsx';
-
-export default function BottomSheet(props) {
-  return <Sheet side="bottom" {...props} />;
-}
--- frontend/src/services/audio/index.js
+++
@@ -1,2 +0,0 @@
-export { default } from './audioService.js';
-export { default as audioService, SOUND_CATALOG } from './audioService.js';
